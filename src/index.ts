891f9434ccde1421d0e71c380610d3f1459b301e
andi faizal <andifaizal@MacBook-Pro-andi.local>
2024-11-28 16:11:56 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 const app = new Hono();
-
-app.get("/home", (c) => c.text("GET /"));
+app.use(logger());
+// app.get("/", (c) => c.text("Hello Hono!"));
 app.post("/", (c) => c.text("POST /"));
 app.put("/", (c) => c.text("PUT /"));
 app.delete("/", (c) => c.text("DELETE /"));
